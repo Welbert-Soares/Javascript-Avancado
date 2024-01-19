57b070ab27c2beabf3d3b147f15ba186a336a9e9
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-01-19 18:48:25 -0300

    reebot patern

--- exercicios/trocar_valores_variaveis.js
+++
@@ -1,9 +0,0 @@
-let a = 7;
-let b = 94;
-
-let c = a;
-a = b;
-b = c;
-
-console.log(a);
-console.log(b);
--- inicio/dados4.js
+++
@@ -1,8 +0,0 @@
-const a = 3;
-let b = 4;
-var c = 5;
-
-
-console.log(a);
-console.log(b);
-console.log(c);
