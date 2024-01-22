4ddc8b68840607b92d141b8dae670ed47e33e605
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-01-22 14:27:10 -0300

    Variaveis e Constantes

---
+++ exercicios-js/fundamentos/variaveisEConstantes.js
@@ -0,0 +1,16 @@
+var a = 3;
+let b = 4;
+
+var a = 30; // var permite redeclarar a variável
+b = 40; // let não permite redeclarar a variável
+
+console.log(a, b);
+
+a = 300;
+b = 400;
+
+console.log(a, b);
+
+const c = 5; // const não permite reatribuir o valor da variável
+
+console.log(c);
