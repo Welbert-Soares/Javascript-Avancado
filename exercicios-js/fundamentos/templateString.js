48423c63d1f370049dd4fca1895cf0b4c2d07241
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-01-22 19:09:14 -0300

    template strings

---
+++ exercicios-js/fundamentos/templateString.js
@@ -0,0 +1,10 @@
+const nome = "Rebeca";
+const concatenacao = 'ola ' + nome + '!';
+const template = `Olá ${nome}!`;
+console.log(concatenacao, template);
+
+// expressoes...
+console.log(`1 + 1 = ${1+1}`);
+
+const up = text => text.toUpperCase();
+console.log(`Ei... ${up('cuidado')}!`);
