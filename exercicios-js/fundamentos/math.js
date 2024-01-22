dcdccb06fcbcd1d7a95b036775dba2d3aa7e11f3
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-01-22 15:00:18 -0300

    math

---
+++ exercicios-js/fundamentos/math.js
@@ -0,0 +1,6 @@
+const raio = 5.6;
+const area = Math.PI * Math.pow(raio, 2); // Math.pow calcula a potência / Math.PI é uma constante que representa o valor de PI
+
+console.log(area);
+console.log(typeof Math); // object
+
