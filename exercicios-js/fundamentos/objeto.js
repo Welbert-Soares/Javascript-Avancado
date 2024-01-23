c82b6706093c205f3dbf73d57c405f60026eb914
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-01-23 14:33:53 -0300

    objeto

---
+++ exercicios-js/fundamentos/objeto.js
@@ -0,0 +1,13 @@
+const prod1 = {}; // par de chaves representa um objeto
+prod1.nome = "Ipone 15 pro"; // nome é um atributo do objeto prod1
+prod1.preco = 5000.00;
+prod1['Desconto black friday'] = 0.40; // evitar atributos com espaço
+
+console.log(prod1);
+
+const prod2 = { 
+    nome: "Camisa polo", 
+    preco: 79.90
+}
+
+console.log(prod2);
