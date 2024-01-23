1f53129136e881598a5cf0ed5f8da6060aa05eec
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-01-23 14:03:35 -0300

    Booleanos

---
+++ exercicios-js/fundamentos/booleanos.js
@@ -0,0 +1,37 @@
+let isAtivo = false;
+console.log(isAtivo);
+
+isAtivo = true;
+console.log(isAtivo);
+
+isAtivo = 1;
+console.log(!!isAtivo); // !! = negação da negação = true
+console.log(!isAtivo); // ! = negação = false
+
+
+console.log('os verdadeiros...');
+console.log(!!3);
+console.log(!!-1);
+console.log(!!" ");
+console.log(!!"texto");
+console.log(!![]);
+console.log(!!{});
+console.log(!!Infinity);
+console.log(!!(isAtivo = true));
+
+console.log('os falsos...');
+console.log(!!0);
+console.log(!!"");
+console.log(!!null);
+console.log(!!NaN);
+console.log(!!undefined);
+console.log(!!(isAtivo = false));
+
+console.log('pra finalizar...');
+console.log(!!('' || null || 0 || ' ')); // || = ou
+console.log(('' || null || 0 || 'epa' || 123));
+
+let nome = 'Lucas';
+console.log(nome || "Desconhecido");
+
+
