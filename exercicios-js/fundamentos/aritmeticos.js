1f049e04a81d6278b10db27910f807849b55f149
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-01-25 16:34:19 -0300

    operadores aritméticos

---
+++ exercicios-js/fundamentos/aritmeticos.js
@@ -0,0 +1,9 @@
+const [a, b, c, d] = [3, 5, 1, 15];
+
+const soma = a + b + c + d;  // soma normal
+const subtracao = d - b; // subtracao normal
+const multiplicacao = a * b; // multiplicacao normal
+const divisao = d / a; // divisao normal
+const modulo = a % 2; // resto da divisao
+
+console.log(soma, subtracao, multiplicacao, divisao, modulo); // 24 10 15 5 1
