711b04b421a842a7e61c10fdf9aa2af0676dc9f5
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-01-30 09:16:27 -0300

    if else: conclusão aula 1

--- exercicios-js/controle/ifElse.js
+++ exercicios-js/controle/ifElse.js
@@ -1,10 +1,11 @@
-const imprimirResultado = function(nota) {
-    if(nota >= 7) {
-        console.log('Aprovado!')
-    } else {
-        console.log('Reprovado!')
-    }
-}
+const imprimirResultado = function (nota) {
+  if (nota >= 7) {
+    console.log("Aprovado!");
+  } else {
+    console.log("Reprovado!");
+  }
+};
 
-imprimirResultado(10)
-imprimirResultado(4)
+imprimirResultado(10);
+imprimirResultado(4);
+imprimirResultado("Epa!"); // Cuidado! JavaScript é fracamente tipado, então não dá erro, mas não é o esperado'
