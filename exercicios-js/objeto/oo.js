fd01825b7eaff9fb41064fabffa40e51561e3f53
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-01-31 18:45:26 -0300

    conceito de objeto

---
+++ exercicios-js/objeto/oo.js
@@ -0,0 +1,25 @@
+// Código não executável!!!
+
+// Procedural
+
+processamento(valor1, valor2, valor3);
+
+// OO
+
+objeto = {
+  valor1,
+  valor2,
+  valor3,
+  processamento() {
+    // ...
+  },
+};
+
+objeto.processamento(); // Foco passou a ser o objeto
+
+// Principios importantes:
+// 1. Abstração ? - Pegar um objeto do mundo real e traduzir para o sistema
+// 2. Encapsulamento - Detalhes de implementação escondidos e mostrar para quem precisa usar o objeto 
+// 3. Herança (prototype) - Relação do tipo é um (ex: um carro é um veículo)
+// 4. Polimorfismo - Multiplas formas de acesso a um objeto (ex: um carro pode ser um sedan, um suv, um hatch, etc)
+
