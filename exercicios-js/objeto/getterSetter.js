8c1b15693b9cbf91e50e02c67b101aa0db1addf5
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-02-01 18:42:02 -0300

    getter setter

---
+++ exercicios-js/objeto/getterSetter.js
@@ -0,0 +1,9 @@
+const sequencia = {
+    _valor: 1, // convenção
+    get valor() { return this._valor++ },
+    set valor(valor) {
+        if(valor > this._valor) {
+            this._valor = valor
+        }
+    }
+}
