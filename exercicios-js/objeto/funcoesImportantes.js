9aa35ef2380c6854aae227bfa4ca1fd85ba8ded9
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-02-02 11:02:04 -0300

    funções importantes

--- exercicios-js/objeto/funcoesImportantes.js
+++ exercicios-js/objeto/funcoesImportantes.js
@@ -17,3 +17,17 @@ Object.defineProperty(pessoa, "dataNascimento", {
     writable: false,
     value: "01/01/2019",
     });
+
+pessoa.dataNascimento = "01/01/2017";
+console.log(pessoa.dataNascimento);
+console.log(Object.keys(pessoa));
+
+// Object.assign (ECMAScript 2015)
+const dest = { a: 1 };
+const o1 = { b: 2 };
+const o2 = { c: 3, a: 4 };
+const obj = Object.assign(dest, o1, o2); // concatena os objetos em um só e sobrescreve os valores iguais 
+
+Object.freeze(obj);
+obj.c = 1234;
+console.log(obj);
