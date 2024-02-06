5c65ec511b5536c5eac53c6b9b0d753bdae609fe
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-02-06 18:58:33 -0300

    filter 3: reconstruindo filter

---
+++ exercicios-js/array/filter2.js
@@ -0,0 +1,20 @@
+Array.prototype.filter2 = function (callback) {
+  const newArray = [];
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i], i, this)) {
+      newArray.push(this[i]);
+    }
+  }
+  return newArray;
+};
+
+const produtos = [
+  { nome: "Notebook", preco: 2499, fragil: true },
+  { nome: "Ipad Pro", preco: 4199, fragil: true },
+  { nome: "Copo de Vidro", preco: 12.49, fragil: true },
+  { nome: "Copo de Plástico", preco: 6.49, fragil: false },
+];
+
+console.log(
+  produtos.filter2((produto) => produto.preco > 2000 && produto.fragil === true)
+);
