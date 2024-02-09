d3cbcaddb3edf268af70518c4bbfeaf3b2d1f1ef
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-02-09 18:59:15 -0300

    flap map

---
+++ exercicios-js/array/flatMap.js
@@ -0,0 +1,17 @@
+const escola = [
+  {
+    nome: "Turma M1",
+    aluno: [
+      {
+        nome: "Gustavo",
+        nota: 8.1,
+      },
+      {
+        nome: "Ana",
+        nota: 9.3,
+      },
+    ],
+  }, {
+     
+  }
+];
