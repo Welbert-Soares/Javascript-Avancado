570bc01db90aa8fae03d724a9339a134c0b56420
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-02-14 16:49:07 -0300

    flatMap

--- exercicios-js/array/flatMap.js
+++ exercicios-js/array/flatMap.js
@@ -1,7 +1,7 @@
 const escola = [
   {
     nome: "Turma M1",
-    aluno: [
+    alunos: [
       {
         nome: "Gustavo",
         nota: 8.1,
@@ -11,7 +11,32 @@ const escola = [
         nota: 9.3,
       },
     ],
-  }, {
-     
-  }
+  },
+  {
+    nome: "Turma M2",
+    alunos: [
+      {
+        nome: "Rebeca",
+        nota: 8.9,
+      },
+      {
+        nome: "Roberto",
+        nota: 7.3,
+      },
+    ],
+  },
 ];
+
+const getNotaDoAluno = (aluno) => aluno.nota;
+const getNotasDaTurma = (turma) => turma.alunos.map(getNotaDoAluno);
+
+const notas1 = escola.map(getNotasDaTurma);
+console.log(notas1);
+
+console.log([].concat([8.1, 9.3], [8.9, 7.3]));
+
+Array.prototype.flatMap = function (callback) {
+  return Array.prototype.concat.apply([], this.map(callback));
+};
+
+const notas2 = escola.flatMap(getNotasDaTurma)
