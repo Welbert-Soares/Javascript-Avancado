3d4097ca934df02af293503b7f1c246a1145f203
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-02-16 14:39:52 -0300

    module cliente

--- exercicios-js/node/moduloCliente.js
+++ exercicios-js/node/moduloCliente.js
@@ -1,12 +1,11 @@
-const moduloA = require('./moduloA')
-const moduleB = require('./moduloB')
+const moduloA = require("./moduloA");
+const moduleB = require("./moduloB");
 
-console.log(moduloA.ola)
-console.log(moduloA.bemVindo)
-console.log(moduloA.ateLogo)
-console.log(moduloA)
+console.log(moduloA.ola);
+console.log(moduloA.bemVindo);
+console.log(moduloA.ateLogo);
+console.log(moduloA);
 
-console.log(moduleB.bomDia)
-console.log(moduleB.boaNoite())
-console.log(moduleB)
-
+console.log(moduleB.bomDia);
+console.log(moduleB.boaNoite());
+console.log(moduleB);
