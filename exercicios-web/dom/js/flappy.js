f3829a766df505226ed8893d208527786dbb4528
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-03-20 18:20:28 -0300

    projeto finalizado

--- exercicios-web/dom/js/flappy.js
+++ exercicios-web/dom/js/flappy.js
@@ -123,6 +123,30 @@ function Progresso() {
   this.atualizarPontos(0);
 }
 
+function estaoSobrepostos(elementoA, elementoB) {
+  const a = elementoA.getBoundingClientRect();
+  const b = elementoB.getBoundingClientRect();
+
+  const horizontal = a.left + a.width >= b.left && b.left + b.width >= a.left;
+  const vertical = a.top + a.height >= b.top && b.top + b.height >= a.top;
+
+  return horizontal && vertical;
+}
+
+function colidiu(passaro, barreiras) {
+    let colidiu = false;
+    barreiras.pares.forEach((parDeBarreiras) => {
+        if (!colidiu) {
+        const superior = parDeBarreiras.superior.elemento;
+        const inferior = parDeBarreiras.inferior.elemento;
+        colidiu =
+            estaoSobrepostos(passaro.elemento, superior) ||
+            estaoSobrepostos(passaro.elemento, inferior);
+        }
+    });
+    return colidiu;
+    }
+
 function FlappyBird() {
     let pontos = 0
 
@@ -151,3 +175,5 @@ function FlappyBird() {
         }, 20)
     }
 }
+
+new FlappyBird().start()
