566ab1ffe398086622f65a636a19ef01f2d8075a
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-03-20 18:36:29 -0300

    gravidade por conta

--- exercicios-web/dom/js/flappy.js
+++ exercicios-web/dom/js/flappy.js
@@ -76,32 +76,44 @@ function Barreiras(altura, largura, abertura, espaco, notificarPonto) {
 }
 
 function Passaro(alturaJogo) {
-  let voando = false;
-
-  this.elemento = novoElemento("img", "passaro");
-  this.elemento.src = "imgs/passaro.png";
-
-  this.getY = () => parseInt(this.elemento.style.bottom.split("px")[0]);
-  this.setY = (y) => (this.elemento.style.bottom = `${y}px`);
-
-  window.onkeydown = (e) => (voando = true);
-  window.onkeyup = (e) => (voando = false);
-
-  this.animar = () => {
-    const novoY = this.getY() + (voando ? 8 : -5);
-    const alturaMaxima = alturaJogo - this.elemento.clientHeight;
-
-    if (novoY <= 0) {
-      this.setY(0);
-    } else if (novoY >= alturaMaxima) {
-      this.setY(alturaMaxima);
-    } else {
-      this.setY(novoY);
-    }
-  };
-
-  this.setY(alturaJogo / 2);
-}
+    let voando = false;
+    let velocidade = 0;
+  
+    this.elemento = novoElemento("img", "passaro");
+    this.elemento.src = "imgs/passaro.png";
+  
+    this.getY = () => parseInt(this.elemento.style.bottom.split("px")[0]);
+    this.setY = (y) => (this.elemento.style.bottom = `${y}px`);
+  
+    window.onkeydown = (e) => {
+      voando = true;
+      velocidade = 6; // Ajuste este valor conforme necessário
+    };
+    window.onkeyup = (e) => {
+      voando = false;
+    };
+  
+    this.animar = () => {
+      if (!voando) {
+        velocidade -= 0.5; // Ajuste este valor conforme necessário
+      }
+  
+      const novoY = this.getY() + velocidade;
+      const alturaMaxima = alturaJogo - this.elemento.clientHeight;
+  
+      if (novoY <= 0) {
+        this.setY(0);
+        velocidade = 0;
+      } else if (novoY >= alturaMaxima) {
+        this.setY(alturaMaxima);
+        velocidade = 0;
+      } else {
+        this.setY(novoY);
+      }
+    };
+  
+    this.setY(alturaJogo / 2);
+  }
 
 // teste 3
 //  const barreiras = new Barreiras(700, 1200, 200, 400)
