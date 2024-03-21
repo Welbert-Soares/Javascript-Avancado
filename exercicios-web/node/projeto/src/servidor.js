d7f4cc7bd5b140a63436deb08f8367893e856cff
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-03-21 20:16:43 -0300

    banco de dadaos api projeto

--- exercicios-web/node/projeto/src/servidor.js
+++ exercicios-web/node/projeto/src/servidor.js
@@ -2,9 +2,22 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bancoDeDados = require('./bancoDeDados')
 
 app.get('/produtos', (req, res, next) => {
-    res.send({nome: 'Notebook', preco: 123.45}) // Converter para JSON
+    res.send(bancoDeDados.getProdutos()) // Converter para JSON
+})
+
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto) // JSON
 })
 
 app.listen(porta, () => {
