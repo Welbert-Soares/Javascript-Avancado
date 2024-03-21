5a3a549d3a702c8f249be06d1effce4eae14d135
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-03-21 11:34:39 -0300

    mulher com menor salario

--- exercicios-web/node/funcionarios/funcionarios.js
+++ exercicios-web/node/funcionarios/funcionarios.js
@@ -1,7 +1,20 @@
 const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
 const axios = require('axios')
 
+const chineses = funcionarios => funcionarios.pais === 'China'
+const mulheres = funcionarios => funcionarios.genero === 'F'
+const menorSalario = (funcionario, funcionarioAtual) => {
+    return funcionario.salario < funcionarioAtual.salario ? funcionario : funcionarioAtual
+}
+
 axios.get(url).then(response => {
     const funcionarios = response.data
     console.log(funcionarios)
+
+    // mulher chinesa com menor salário?
+    const funcionario = funcionarios
+        .filter(chineses)
+        .filter(mulheres)
+        .reduce(menorSalario) // reduce() é uma função que serve para transformar um array em um único elemento
+    console.log(funcionario)
 })
