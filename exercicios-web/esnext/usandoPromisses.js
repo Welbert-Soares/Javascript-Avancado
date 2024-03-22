26efe73519ea00096eec6bffa2d3aef3adbfd446
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-03-22 16:19:36 -0300

    usando Promisses

---
+++ exercicios-web/esnext/usandoPromisses.js
@@ -0,0 +1,45 @@
+// com promisse
+const { rejects } = require("assert");
+const http = require("http");
+const { resolve } = require("path");
+
+const getTurma = (letra) => {
+  const url = `http://files.cod3r.com.br/curso-js/turma${letra}.json`;
+  return new Promise((resolve, rejects) => {
+    http.get(url, (res) => {
+      let resultado = "";
+
+      res.on("data", (dados) => {
+        resultado += dados;
+      });
+
+      res.on("end", () => {
+        try {
+          resolve(JSON.parse(resultado));
+        } catch (e) {
+          reject(e);
+        }
+      });
+    });
+  });
+};
+
+//    let nomes = [];
+//   getTurma("A").then((alunos) => {
+//   nomes = nomes.concat(alunos.map((a) => `A: ${a.nome}`));
+//  getTurma("B").then((alunos) => {
+//      nomes = nomes.concat(alunos.map((a) => `B: ${a.nome}`));
+//      getTurma("C").then((alunos) => {
+//       nomes = nomes.concat(alunos.map((a) => `C: ${a.nome}`));
+//       console.log(nomes);
+//      });
+//   });
+//    });
+
+Promise.all([getTurma("A"), getTurma("B"), getTurma("C")])
+  .then((turmas) => [].concat(...turmas))
+  .then((alunos) => alunos.map((aluno) => aluno.nome))
+  .then((nomes) => console.log(nomes))
+  .catch((e) => console.log(e.message)); // catch error
+
+getTurma("D").catch((e) => console.log(e.message)); // catch error
